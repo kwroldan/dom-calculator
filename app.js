0c9bae6c2c51b26bedc509b738eabcf281ae5431
Kristofer <kroldan@ford.com>
2022-04-19 12:05:33 -0400

    Add new function and prevent changing text content for multiplication and division operators

--- app.js
+++ app.js
@@ -1,18 +1,23 @@
 const calculatorScreen = document.querySelector("#screen");
 const allButtons = document.querySelectorAll("span");
 
-// Operator Variables
-const divisionOperator = document.querySelector(".operator:nth-child(2)");
-divisionOperator.innerText = "/"
-const multiplicationOperator = document.querySelector(".operator:nth-child(3)");
-multiplicationOperator.innerText = "*"
+function operatorChange(equation){
+    let newEquation = equation
+    if (equation.includes("÷")) {
+        newEquation = equation.replace("÷", "/")
+    }
+    else if(equation.includes("x")) {
+        newEquation = equation.replace("x", "*")
+    }
+    return newEquation
+}
 
 allButtons.forEach(button => button.addEventListener("click", function() {
     if (button.innerText !== "=") {
         calculatorScreen.innerText += button.innerText
     };
     if (button.innerText === "=") {
-        calculatorScreen.innerText = eval(calculatorScreen.innerText)
+        calculatorScreen.innerText = eval(operatorChange(calculatorScreen.innerText))
     }
     if (button.innerText === "C") {
         calculatorScreen.innerText = ""
